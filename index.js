dad83c8f47b9af3ed7e622a7c6d25cddecb11c1b
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-06-12 12:35:59 -0700

    Done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const graduate = ["Charlie", "Samip", "Ali"];
+
+function writeCards (names, event) {
+    const graduationGreeting = [];
+    for (let i = 0; i < names.length; i++) {
+       const graduationMessage = `Thank you, ${graduate[i]}, for the wonderful ${event} gift!`;
+       graduationGreeting.push(graduationMessage);
+    }
+    return graduationGreeting;
+
+}
+
+console.log(writeCards(graduate,"birthday"));
+
+
+function countDown (x) {
+    while (x >= 0) {
+        console.log(x);
+        x -= 1;
+    }
+
+}
+countDown(10);
